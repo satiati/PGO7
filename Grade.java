5aecf782539194350b614297806d1bac15edb9bb
satiati <162111295+satiati@users.noreply.github.com>
2024-05-06 09:41:31 +0200

    added Grade.java

---
+++ Grade.java
@@ -0,0 +1,9 @@
+public class Grade {
+    private int value;
+    private String subjectName;
+
+    public Grade(int value, String subjectName) {
+        this.value = value;
+        this.subjectName = subjectName;
+    }
+}
